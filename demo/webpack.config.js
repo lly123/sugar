6a7c18227d44bc89cc1d311883a05d451fb7fb83
richard.li <richard.li@blackboard.com>
2016-04-28 17:45:11 +0800

    refactor web pack configuration

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -6,12 +6,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event;
 
-const common = {};
+const common = {
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8080',
+        path.join(__dirname, 'src/web/main.js')
+    ],
+    output: {
+        path: path.join(__dirname, 'build/web'),
+        publicPath: './',
+        filename: 'main.js'
+    },
+    target: 'web',
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'socket.io-client': 'io',
+        'socket.io': 'io'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?' + JSON.stringify({
+                    presets: ['es2015', 'react']
+                })],
+                include: [
+                    path.join(__dirname, 'src/web')
+                ]
+            },
+
+            {
+                test: /\.styl/,
+                loader: 'style!css!stylus'
+            }
+        ]
+    }
+};
 
 if (TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
         devServer: {
-            contentBase: path.join(__dirname, 'build'),
+            contentBase: path.join(__dirname, 'build/web'),
             // Enable history API fallback so HTML5 History API based
             // routing works. This is a good default that will come
             // in handy in more complicated setups.
@@ -21,7 +56,7 @@ if (TARGET === 'start' || !TARGET) {
             progress: true,
 
             // Display only errors to reduce the amount of output.
-            stats: 'errors-only',
+            // stats: 'errors-only',
 
             // Parse host and port from env so this is easy to customize.
             //
@@ -33,7 +68,6 @@ if (TARGET === 'start' || !TARGET) {
             host: process.env.HOST,
             port: process.env.PORT || 8080
         },
-
         plugins: [
             new webpack.HotModuleReplacementPlugin()
         ]
@@ -42,62 +76,27 @@ if (TARGET === 'start' || !TARGET) {
 
 if (TARGET === 'build-web') {
     module.exports = merge(common, {
-        entry: {
-            // "app" : path.join(__dirname, 'src/server/app.js'),
-            "main": path.join(__dirname, 'src/web/main.js')
-        },
-        output: {
-            path: path.join(__dirname, 'build'),
-            publicPath: './',
-            filename: '[name].js'
-        },
-        target: 'web',
-        externals: {
-            'react': 'React',
-            'react-dom': 'ReactDOM',
-            'socket.io-client': 'io',
-            'socket.io': 'io'
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.jsx?$/,
-                    loaders: ['babel?' + JSON.stringify({
-                        presets: ['es2015', 'react']
-                    })],
-                    include: [
-                        path.join(__dirname, 'src')
-                    ]
-                },
-
-                {
-                    test: /\.styl/,
-                    loader: 'style!css!stylus'
-                }
-            ]
-        },
         plugins: [
-            new CopyWebpackPlugin([
-                {from: 'bower_components/react/**/*'},
-                {from: 'bower_components/socket.io-client/**/*'}
-            ]),
             new HtmlWebpackPlugin({
                 filename: 'index.html',
                 template: 'src/web/index.html',
-                excludeChunks: ['app'],
                 hash: true
-            })
+            }),
+            new CopyWebpackPlugin([
+                {from: 'bower_components/react/**/*'},
+                {from: 'bower_components/socket.io-client/**/*'}
+            ])
         ]
     });
 }
 
 if (TARGET === 'build-server') {
-    module.exports = merge(common, {
+    module.exports = {
         entry: {
             "app": path.join(__dirname, 'src/server/app.js')
         },
         output: {
-            path: path.join(__dirname, 'build'),
+            path: path.join(__dirname, 'build/server'),
             publicPath: './',
             filename: '[name].js'
         },
@@ -111,10 +110,10 @@ if (TARGET === 'build-server') {
                         presets: ['es2015', 'react']
                     })],
                     include: [
-                        path.join(__dirname, 'src')
+                        path.join(__dirname, 'src/server')
                     ]
                 }
             ]
         }
-    });
+    };
 }
